375e4f9d6f82a5de7760fb4076a6513cb3f5b02c
Tristan <LeoDog896@hotmail.com>
2022-04-21 20:41:48 -0400

    Begin shadow decryptor

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use clap::Parser;
-use std::{fs::read_to_string, io::BufReader};
+use std::{fs::File, io::{BufReader, BufRead}};
 use anyhow::Result;
+use regex::Regex;
+use crypt3::crypt;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     /// Name of the person to greet
-    #[clap(short, long)]
     file: std::path::PathBuf,
 }
 
@@ -17,13 +18,30 @@ fn main() {
 
 fn main_error() -> Result<()> {
     let args = Args::parse();
+    let regex = Regex::new(r"(\w+):((\$\d\$[\w]+)\$([./\d\w]+)|\*|\!):")?;
 
-    let reader = BufReader::new(args.file);
-    let lines: Vec<_> = reader.lines().collect();
+    let file = File::open(args.file)?;
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+      let new_line = line?;
+      let unprocessed_result = regex.captures(&new_line);
 
-  for l in lines {
-      ...
-  }
+      if let Some(unprocessed_result) = unprocessed_result {
+        let username = unprocessed_result.get(1).unwrap().as_str();
+        let encrypted_password = unprocessed_result.get(2).unwrap().as_str();
+
+        match encrypted_password {
+          "!" => println!("User {} is a locked user account", username),
+          "*" => println!("User {} is a locked servie account", username),
+          pswd => {
+            let digest = crypt(b"abcdefghijklmnop", b"$1$");
+            //println!("{}", pswd);
+          }
+        }
+      } else {
+        println!("Bad format: {}", new_line);
+      }
+    }
 
     Ok(())
 }
